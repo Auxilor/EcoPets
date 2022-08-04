a0381e113c753bb6e21eb8191867e8e560c8e2d3
Auxilor <william.favierparsons1@gmail.com>
2022-08-04 12:34:42 +0100

    effects

--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecopets/commands/CommandEcopets.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecopets/commands/CommandEcopets.kt
@@ -1,16 +1,18 @@
 package com.willfp.ecopets.commands
 
-import com.willfp.eco.core.EcoPlugin
 import com.willfp.eco.core.command.impl.PluginCommand
+import com.willfp.libreforge.CommandFixLingeringEffects
+import com.willfp.libreforge.LibReforgePlugin
 import org.bukkit.command.CommandSender
 
-class CommandEcopets(plugin: EcoPlugin) : PluginCommand(plugin, "ecopets", "ecopets.command.ecopets", false) {
+class CommandEcopets(plugin: LibReforgePlugin) : PluginCommand(plugin, "ecopets", "ecopets.command.ecopets", false) {
     init {
         this.addSubcommand(CommandReload(plugin))
             .addSubcommand(CommandGive(plugin))
             .addSubcommand(CommandGiveEgg(plugin))
             .addSubcommand(CommandGiveXP(plugin))
             .addSubcommand(CommandReset(plugin))
+            .addSubcommand(CommandFixLingeringEffects(plugin, "ecopets.command.fixlingeringeffects"))
     }
 
     override fun onExecute(sender: CommandSender, args: List<String>) {
