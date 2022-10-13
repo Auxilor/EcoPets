3cd800aeacd9e37c3dd470452cab46c7c45c8878
Auxilor <william.favierparsons1@gmail.com>
2022-10-13 20:13:29 +0100

    libreforge-updater

--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecopets/commands/CommandEcopets.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecopets/commands/CommandEcopets.kt
@@ -1,16 +1,29 @@
 package com.willfp.ecopets.commands
 
-import com.willfp.eco.core.EcoPlugin
 import com.willfp.eco.core.command.impl.PluginCommand
+import com.willfp.ecopets.pets.Pets
+import com.willfp.libreforge.LibReforgePlugin
+import com.willfp.libreforge.lrcdb.CommandExport
+import com.willfp.libreforge.lrcdb.CommandImport
+import com.willfp.libreforge.lrcdb.ExportableConfig
 import org.bukkit.command.CommandSender
 
-class CommandEcopets(plugin: EcoPlugin) : PluginCommand(plugin, "ecopets", "ecopets.command.ecopets", false) {
+class CommandEcopets(plugin: LibReforgePlugin) : PluginCommand(plugin, "ecopets", "ecopets.command.ecopets", false) {
     init {
         this.addSubcommand(CommandReload(plugin))
             .addSubcommand(CommandGive(plugin))
             .addSubcommand(CommandGiveEgg(plugin))
             .addSubcommand(CommandGiveXP(plugin))
             .addSubcommand(CommandReset(plugin))
+            .addSubcommand(CommandImport("pets", plugin))
+            .addSubcommand(CommandExport(plugin) {
+                Pets.values().map {
+                    ExportableConfig(
+                        it.id,
+                        it.config
+                    )
+                }
+            })
     }
 
     override fun onExecute(sender: CommandSender, args: List<String>) {
