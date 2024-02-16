3a3bdc3154608564e22ae3ead5a0d19e30494536
Daniel Zhang <xiaozhangshiw@163.com>
2024-02-16 21:53:46 +0800

    Add ItemStackPetEntity

--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecopets/pets/entity/ItemStackPetEntity.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecopets/pets/entity/ItemStackPetEntity.kt
@@ -1,21 +1,22 @@
 package com.willfp.ecopets.pets.entity
 
-import com.willfp.eco.core.items.builder.SkullBuilder
+import com.willfp.eco.core.items.Items
 import com.willfp.ecopets.pets.Pet
 import org.bukkit.Location
 import org.bukkit.entity.ArmorStand
 import org.bukkit.inventory.ItemStack
 
-class ItemStackPetEntity(pet: Pet) : PetEntity(pet) {
+class ItemStackPetEntity(
+    pet: Pet,
+    private val itemStack: String
+) : PetEntity(pet) {
     override fun spawn(location: Location): ArmorStand {
         val stand = emptyArmorStandAt(location, pet)
 
-        val skull: ItemStack = SkullBuilder()
-            .setSkullTexture(pet.entityTexture)
-            .build()
+        val itemStack: ItemStack = Items.lookup(itemStack).item
 
         @Suppress("UNNECESSARY_SAFE_CALL") // Can be null.
-        stand.equipment?.helmet = skull
+        stand.equipment?.helmet = itemStack
 
         return stand
     }
