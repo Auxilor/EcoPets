7b06f1d517b5bc12165918b9adf20c353fd1d22a
NicoNeko <nicuch100@gmail.com>
2026-03-07 20:30:50 +0200

    Revert "Reload on main/global thread"
    This reverts commit 5aa7df0a0f0be180466801cc0a33568a0acc8f25.

--- eco-core/core-plugin/src/main/kotlin/com/willfp/ecopets/commands/CommandReload.kt
+++ eco-core/core-plugin/src/main/kotlin/com/willfp/ecopets/commands/CommandReload.kt
@@ -1,6 +1,5 @@
 package com.willfp.ecopets.commands
 
-import com.willfp.eco.core.Prerequisite
 import com.willfp.eco.core.command.impl.Subcommand
 import com.willfp.ecopets.plugin
 import org.bukkit.command.CommandSender
@@ -12,13 +11,7 @@ object CommandReload : Subcommand(
     false
 ) {
     override fun onExecute(sender: CommandSender, args: List<String>) {
-        val runnable: Runnable = {
-            plugin.reload()
-            sender.sendMessage(plugin.langYml.getMessage("reloaded"))
-        }
-        if (Prerequisite.HAS_FOLIA.isMet)
-            plugin.scheduler.runTask(runnable) // run on global thread
-        else
-            runnable.run()
+        plugin.reload()
+        sender.sendMessage(plugin.langYml.getMessage("reloaded"))
     }
 }
